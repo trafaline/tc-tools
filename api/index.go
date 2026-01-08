1927185a87f71bb3f5269c5118971e1e5107d386
dedearya96 <trafaline.official@gmail.com>
2026-01-08 20:42:33 +0000

    u

--- api/index.go
+++ api/index.go
@@ -1,3 +1,39 @@
+// package main
+
+// import (
+// 	"log"
+// 	"os"
+// 	"tc-tools/controllers"
+// 	"tc-tools/middleware"
+
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/joho/godotenv"
+// )
+
+// func main() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// 	r := gin.Default()
+
+// 	r.Use(middleware.CORSMiddleware())
+
+// 	api := r.Group("/api/v1")
+// 	{
+// 		api.Use(middleware.AuthMiddleware())
+
+// 		api.POST("/url-preview", controllers.HandlePreview)
+// 	}
+
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = "8080"
+// 	}
+// 	r.Run(":" + port)
+// }
+
 package handler // Nama package harus handler untuk folder api/ di Vercel
 
 import (
