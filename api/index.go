a458f35ea20d7e2f503b450152f33599fd07f01b
dedearya96 <trafaline.official@gmail.com>
2026-01-08 20:41:08 +0000

    u

--- api/index.go
+++ api/index.go
@@ -1,35 +1,34 @@
-package main
+package handler // Nama package harus handler untuk folder api/ di Vercel
 
 import (
-	"log"
-	"os"
+	"net/http"
 	"tc-tools/controllers"
 	"tc-tools/middleware"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+// Declare r sebagai variable global agar tidak di-init ulang terus (Optimization)
+var r *gin.Engine
 
-	r := gin.Default()
+func init() {
+	// Pindahkan konfigurasi Gin ke sini
+	gin.SetMode(gin.ReleaseMode)
+	r = gin.Default()
 
+	// Pasang Middleware
 	r.Use(middleware.CORSMiddleware())
 
+	// Pasang Route
 	api := r.Group("/api/v1")
 	{
 		api.Use(middleware.AuthMiddleware())
-
 		api.POST("/url-preview", controllers.HandlePreview)
 	}
+}
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	r.Run(":" + port)
+// Handler adalah fungsi yang WAJIB ada dan di-Export (Huruf Kapital)
+func Handler(w http.ResponseWriter, req *http.Request) {
+	// Vercel akan memanggil fungsi ini, lalu kita teruskan ke Gin
+	r.ServeHTTP(w, req)
 }
